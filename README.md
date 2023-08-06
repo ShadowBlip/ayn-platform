50321dae5ddace4492ad56b54dda7dc3a0633d45
Derek J. Clark <derekjohn.clark@gmail.com>
2023-08-06 10:58:33 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# ayn-platform
-Linux drivers for AYN x86 handhelds providing a hwmon interface for pwm control, RGB Control and temperature sensors.
+# Platform driver for AYN x86 handheldsds
+
+This driver provides a hwmon interface for PWM control, as well as RGB
+control and access to temperature sensotrs provided by the system EC
+
+
+Supported devices include:
+
+ - AYN Loki Max
+
+## Build
+If you only want to build and test the module (you need headers for your
+kernel):
+
+```shell
+$ git clone https://github.com/ShadowBlip/ayn-platform.git
+$ cd ayn-platform
+$ make
+```
+
+Then insert the module and check `sensors` and `dmesg` if appropriate:
+```shell
+# insmod ayn-sensors.ko
+$ sensors
+```
+
+## Install
+
+You'll need appropriate headers for your kernel and `dkms` package from your
+distribution.
+
+```shell
+$ git clone https://github.com/ShadowBlip/ayn-platform.git
+$ cd ayn-platform
+$ make
+# make dkms
+```
+
+## Usage
+
+Insert the module with `insmod`. Then look for a `hwmon` device with name
+`aynec`, i.e.:
+
+`$ cat /sys/class/hwmon/hwmon?/name`
+
+### Reading fan RPM
+
+`sensors` will show the fan RPM as read from the EC. You can also read the
+file `fan1_input` to get the fan RPM.
+
+### Controlling the fan
+
+***Warning: controlling the fan without an accurate reading of the CPU, GPU,
+and Battery temperature can cause irreversible damage to the device. Use at
+your own risk!***
+
+To enable manual control of the fan (assuming `hwmon5` is our driver, look for
+`aynec` in the `name` file):
+
+`# echo 1 > /sys/class/hwmon/hwmon5/pwm1_enable`
+
+Then input values in the range `[0-255]` to the pwm:
+
+`# echo 100 > /sys/class/hwmon/hwmon5/pwm1`
+
---
+++ ayn-sensors.c
@@ -0,0 +1,328 @@
+// SPDX-License-Identifier: GPL-3.0+
+/*
+ * Platform driver for Ayn x86 Handhelds that expose fan reading and
+ * control via hwmon sysfs, as well as temperature sensor readings 
+ * exposed by the EC and RGB control via platform sysfs.
+ *
+ * Fan control is provided via pwm interface in the range [0-255].
+ * They use [0-100] as range in the EC, the written value is
+ * scaled to accommodate for that.
+ *
+ * Copyright (C) 2023 Derek J. Clark <derekjohn.clark@gmail.com>
+ */
+
+#include <linux/acpi.h>
+#include <linux/dmi.h>
+#include <linux/hwmon.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/processor.h>
+
+/* Handle ACPI lock mechanism */
+static u32 ayn_mutex;
+
+#define ACPI_LOCK_DELAY_MS	500
+
+static bool lock_global_acpi_lock(void)
+{
+	return ACPI_SUCCESS(acpi_acquire_global_lock(ACPI_LOCK_DELAY_MS, &ayn_mutex));
+}
+
+static bool unlock_global_acpi_lock(void)
+{
+	return ACPI_SUCCESS(acpi_release_global_lock(ayn_mutex));
+}
+
+enum ayn_model {
+};
+
+static enum ayn_model model;
+
+/* EC Teperature Sensors */
+/* TODO:
+ */
+#define AYN_SENSOR_TEMP_0_REG		0x04 /* Battery */
+#define AYN_SENSOR_TEMP_1_REG		0x05 /* Mothermodel */
+#define AYN_SENSOR_TEMP_2_REG		0x06 /* unknown */
+#define AYN_SENSOR_TEMP_3_REG		0x07 /* Charger IC */
+#define AYN_SENSOR_TEMP_4_REG		0x08 /* vCore */
+#define AYN_SENSOR_TEMP_5_REG		0x09 /* CPU Core */
+#define AYN_SENSOR_TEMP_6_REG		0x0A /* unknown */
+#define AYN_SENSOR_TEMP_7_REG		0x0B /* unknown */
+
+/* Fan reading and PWM */
+#define AYN_SENSOR_PWM_FAN_SPEED_REG		0x20 /* Fan speed reading is 2 registers long */
+#define AYN_SENSOR_PWM_FAN_MODE_REG		0x10 /* PWM operating mode */
+#define AYN_SENSOR_PWM_FAN_SET_REG		0x11 /* PWM duty cycle */
+
+/* EC controlled fan curve registers */
+/* TODO:
+ * pwm1_auto_point[1-5]_pwm
+ * pwm1_auto_point[1-5]_temp 
+ */
+#define AYN_SENSOR_PWM_FAN_SPEED_1_REG		0x12  
+#define AYN_SENSOR_PWM_FAN_SPEED_2_REG		0x14  
+#define AYN_SENSOR_PWM_FAN_SPEED_3_REG		0x16  
+#define AYN_SENSOR_PWM_FAN_SPEED_4_REG		0x18  
+#define AYN_SENSOR_PWM_FAN_SPEED_5_REG		0x1A  
+#define AYN_SENSOR_PWM_FAN_TEMP_1_REG		0x13  
+#define AYN_SENSOR_PWM_FAN_TEMP_2_REG		0x15  
+#define AYN_SENSOR_PWM_FAN_TEMP_3_REG		0x17  
+#define AYN_SENSOR_PWM_FAN_TEMP_4_REG		0x19  
+#define AYN_SENSOR_PWM_FAN_TEMP_5_REG		0x1B  
+
+
+/* EC Controlled PWM RGB registers */
+#define AYN_SENSOR_RGB_R_REG		0xB0 /* PWM Red Duty cycle, range 0x00-0xFF */
+#define AYN_SENSOR_RGB_G_REG		0xB1 /* PWM Green Duty cycle, range 0x00-0xFF */
+#define AYN_SENSOR_RGB_B_REG		0xB2 /* PWM Blue Duty cycle, range 0x00-0xFF */
+#define AYN_SENSOR_RGB_MODE_REG		0xB3 /* RGB PWM Mode */
+
+static const struct dmi_system_id dmi_table[] = {
+	{
+		.matches = {
+			DMI_MATCH(DMI_BOARD_VENDOR, "AYN"),
+			DMI_EXACT_MATCH(DMI_BOARD_NAME, "Loki Max"),
+		},
+		.driver_data = (void *)ayn_loki_max,
+	},
+	{},
+};
+
+/* Helper functions to handle EC read/write */
+static int read_from_ec(u8 reg, int size, long *val)
+{
+	int i;
+	int ret;
+	u8 buffer;
+
+	if (!lock_global_acpi_lock())
+		return -EBUSY;
+
+	*val = 0;
+	for (i = 0; i < size; i++) {
+		ret = ec_read(reg + i, &buffer);
+		if (ret)
+			return ret;
+		*val <<= i * 8;
+		*val += buffer;
+	}
+
+	if (!unlock_global_acpi_lock())
+		return -EBUSY;
+
+	return 0;
+}
+
+static int write_to_ec(u8 reg, u8 value)
+{
+	int ret;
+
+	if (!lock_global_acpi_lock())
+		return -EBUSY;
+
+	ret = ec_write(reg, value);
+
+	if (!unlock_global_acpi_lock())
+		return -EBUSY;
+
+	return ret;
+}
+
+/* PWM mode functions */
+/* Manual provides direct control of the PWM */
+static int ayn_pwm_manual(void)
+{
+	return write_to_ec(AYN_SENSOR_PWM_FAN_MODE_REG, 0x00);
+}
+
+/* Auto provides EC full control of the PWM */
+static int ayn_pwm_auto(void)
+{
+	return write_to_ec(AYN_SENSOR_PWM_FAN_MODE_REG, 0x01);
+}
+
+/* User defined mode allows users to set a custom 5 point 
+ * fan curve in the EC which uses the CPU temperature. */
+static int ayn_pwm_user(void)
+{
+	return write_to_ec(AYN_SENSOR_PWM_FAN_MODE_REG, 0x02);
+}
+
+/* Callbacks for hwmon interface */
+static umode_t ayn_ec_hwmon_is_visible(const void *drvdata,
+				       enum hwmon_sensor_types type, u32 attr, int channel)
+{
+	switch (type) {
+	case hwmon_fan:
+		return 0444;
+	case hwmon_pwm:
+		return 0644;
+	default:
+		return 0;
+	}
+}
+
+static int ayn_platform_read(struct device *dev, enum hwmon_sensor_types type,
+			     u32 attr, int channel, long *val)
+{
+	int ret;
+
+	switch (type) {
+	case hwmon_fan:
+		switch (attr) {
+		case hwmon_fan_input:
+			return read_from_ec(AYN_SENSOR_PWM_FAN_SPEED_REG, 2, val);
+		default:
+			break;
+		}
+		break;
+	case hwmon_pwm:
+		switch (attr) {
+		case hwmon_pwm_input:
+			ret = read_from_ec(AYN_SENSOR_PWM_FAN_SET_REG, 1, val);
+			if (ret)
+				return ret;
+			switch (model) {
+			case ayn_loki_max:
+				*val = (*val * 255) / 100;
+				break;
+			default:
+				break;
+			}
+			return 0;
+		case hwmon_pwm_enable:
+			return read_from_ec(AYN_SENSOR_PWM_FAN_MODE_REG, 1, val);
+		default:
+			break;
+		}
+		break;
+	default:
+		break;
+	}
+	return -EOPNOTSUPP;
+}
+
+static int ayn_platform_write(struct device *dev, enum hwmon_sensor_types type,
+			      u32 attr, int channel, long val)
+{
+	switch (type) {
+	case hwmon_pwm:
+		switch (attr) {
+		case hwmon_pwm_enable:
+			if (val == 1)
+				return ayn_pwm_manual();
+			else if (val == 2)
+				return ayn_pwm_user();
+			else if (val == 0)
+				return ayn_pwm_auto();
+			return -EINVAL;
+		case hwmon_pwm_input:
+			if (val < 0 || val > 255)
+				return -EINVAL;
+			switch (model) {
+			case ayn_loki_max:
+				val = (val * 100) / 255;
+				break;
+			default:
+				break;
+			}
+			return write_to_ec(AYN_SENSOR_PWM_FAN_SET_REG, val);
+		default:
+			break;
+		}
+		break;
+	default:
+		break;
+	}
+	return -EOPNOTSUPP;
+}
+
+/* Known sensors in the AYN EC controllers */
+static const struct hwmon_channel_info * const ayn_platform_sensors[] = {
+	HWMON_CHANNEL_INFO(fan,
+			   HWMON_F_INPUT),
+	HWMON_CHANNEL_INFO(pwm,
+			   HWMON_PWM_INPUT | HWMON_PWM_ENABLE),
+	NULL,
+};
+/*
+static struct attribute *ayn_ec_attrs[] = {
+	&dev_attr_tt_toggle.attr,
+	NULL
+};
+*/
+ATTRIBUTE_GROUPS(ayn_ec);
+
+static const struct hwmon_ops ayn_ec_hwmon_ops = {
+	.is_visible = ayn_ec_hwmon_is_visible,
+	.read = ayn_platform_read,
+	.write = ayn_platform_write,
+};
+
+static const struct hwmon_chip_info ayn_ec_chip_info = {
+	.ops = &ayn_ec_hwmon_ops,
+	.info = ayn_platform_sensors,
+};
+
+/* Initialization logic */
+static int ayn_platform_probe(struct platform_device *pdev)
+{
+	const struct dmi_system_id *dmi_entry;
+	struct device *dev = &pdev->dev;
+	struct device *hwdev;
+	int ret;
+
+	dmi_entry = dmi_first_match(dmi_table);
+	model = (enum ayn_model)(unsigned long)dmi_entry->driver_data;
+
+	switch (model) {
+	case ayn_loki_max:
+		ret = devm_device_add_groups(dev, ayn_ec_groups);
+		if (ret)
+			return ret;
+		break;
+	default:
+		break;
+	}
+
+	hwdev = devm_hwmon_device_register_with_info(dev, "aynec", NULL,
+						     &ayn_ec_chip_info, NULL);
+
+	return PTR_ERR_OR_ZERO(hwdev);
+}
+
+static struct platform_driver ayn_platform_driver = {
+	.driver = {
+		.name = "ayn-platform",
+	},
+	.probe = ayn_platform_probe,
+};
+
+static struct platform_device *ayn_platform_device;
+
+static int __init ayn_platform_init(void)
+{
+	ayn_platform_device =
+		platform_create_bundle(&ayn_platform_driver,
+				       ayn_platform_probe, NULL, 0, NULL, 0);
+
+	return PTR_ERR_OR_ZERO(ayn_platform_device);
+}
+
+static void __exit ayn_platform_exit(void)
+{
+	platform_device_unregister(ayn_platform_device);
+	platform_driver_unregister(&ayn_platform_driver);
+}
+
+MODULE_DEVICE_TABLE(dmi, dmi_table);
+
+module_init(ayn_platform_init);
+module_exit(ayn_platform_exit);
+
+MODULE_AUTHOR("Derek John Clark <derekjohn.clark@gmail.com>");
+MODULE_DESCRIPTION("Platform driver that handles EC sensors of Ayn x86 devices");
+MODULE_LICENSE("GPL");
